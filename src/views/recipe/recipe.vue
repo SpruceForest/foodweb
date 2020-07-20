cee5fe4a0f358640eeb50454a86c7531400b71ef
SpruceForest <1801122093@qq.com>
2020-07-20 15:30:26 +0800

    菜谱大全样式修改

--- src/views/recipe/recipe.vue
+++ src/views/recipe/recipe.vue
@@ -195,42 +195,34 @@
     .recipe-link
         font-size 0;
         margin-top 5px
-
         a
             display inline-block
             font-size 12px
             padding 0px 8px
             height 28px
             line-height 28px
-
         .active
             background #ff3232
             color #fff
-
     .recipe
         h2
             text-align center
             line-height 150px
-
         .el-main
             padding 0
-
         .filter-box
             background #fff
             padding 10px
             width 100%
             float left
             box-sizing border-box
-
     .filter-tags
         display flex
         flex-wrap wrap
         justify-content space-around
-
         .tag-selected
             background-color #ff3232
             color #fff
-
     .menu-empty
         width 100%
         text-align center
